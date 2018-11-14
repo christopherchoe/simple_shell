5603f5c8699cb3cd14413304b7fdc8233b045120
christopherchoe <chchchoe@gmail.com>
2018-11-14 19:17:54 +0000

    fixed betty issues

--- simple_shell/test/free.c
+++ simple_shell/test/free.c
@@ -1,16 +1,28 @@
 #include "shell.h"
 
+/**
+  * free_call - frees a linked list
+  *
+  * @head: pointer to the first node of the linked list
+  * Return: void
+  */
 void free_call(free_list *head)
 {
 	if (!head)
 		return;
-	while(head)
+	while (head)
 	{
 		free(head->mall);
 		head = head->next;
 	}
 }
 
+/**
+  * free_double - frees a 2D array
+  *
+  * @arglist: the double array to free
+  * Return: void
+  */
 void free_double(char **arglist)
 {
 	int i = 0;
